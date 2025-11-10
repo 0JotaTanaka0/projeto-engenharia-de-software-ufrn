f936ba41081fb921f87efd0ee311c142e1cd0b4f
JotaTanaka <141186393+0JotaTanaka0@users.noreply.github.com>
2025-11-10 09:41:41 -0300

    Update agendar.js

--- Tarefa_MVP/agendar.js
+++ Tarefa_MVP/agendar.js
@@ -21,7 +21,7 @@ form.addEventListener('submit', (e) => {
     atualizarLista();
     form.reset();
 
-    alert('✅ Consulta agendada com sucesso!');
+    alert('Consulta agendada com sucesso!');
 });
 
 function atualizarLista() {
@@ -36,9 +36,9 @@ function atualizarLista() {
         const li = document.createElement('li');
         li.innerHTML = `
             <strong>${consulta.especialista}</strong><br>
-            📅 ${consulta.data} — 🕒 ${consulta.hora}<br>
-            ${consulta.sintomas ? '💬 ' + consulta.sintomas + '<br>' : ''}
-            📞 ${consulta.contato}
+            ${consulta.data} — ${consulta.hora}<br>
+            ${consulta.sintomas ? ' ' + consulta.sintomas + '<br>' : ''}
+            ${consulta.contato}
             <button class="cancelar" onclick="cancelarConsulta(${index})">
                 <i class='bx bx-x'></i> Cancelar
             </button>
